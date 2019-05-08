81e93c485662ce84451c959e41c3f0173d6ed6d7
alexey <alexeyorc@gmail.com>
2019-05-08 22:36:03 +0300

    Updated html-saver app. Added TextArea adn Display button.

--- html-saver/src/html/saver/ui/App.java
+++ html-saver/src/html/saver/ui/App.java
@@ -5,9 +5,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -16,18 +18,17 @@ import java.net.URL;
 import java.net.URLConnection;
 
 
-// TODO: Create TextArea...
-// Download and  Save buttons...
-
 public class App extends Application {
-    private static final double MIN_WIDTH = 600.0f;
-    private static final double MIN_HEIGHT = 400.0f;
+    private static final double MIN_WIDTH = 800.0f;
+    private static final double MIN_HEIGHT = 600.0f;
     private static final String STYLE = "-fx-text-fill: black; -fx-font-size: 18; -fx-font-family: Roboto Material Thin";
 
     private Stage appStage;
     private Label label;
     private TextField field;
     private Button save;
+    private Button display;
+    private TextArea textArea;
     private GridPane rootGrid;
 
     public void start(Stage primaryStage) {
@@ -42,15 +43,26 @@ public class App extends Application {
         prepareAppStage();
         prepareLabel();
         prepareField();
+        prepareTextArea();
         prepareSaveButton();
+        prepareDisplayButton();
         prepareRootGrid();
     }
 
     private void prepareRootGrid() {
         rootGrid = new GridPane();
-        rootGrid.add(label, 0, 0);
-        rootGrid.add(field, 1, 0);
-        rootGrid.add(save, 2, 0);
+        HBox input = new HBox();
+        input.getChildren().addAll(label, field);
+        input.setAlignment(Pos.CENTER);
+
+        HBox buttons = new HBox();
+        buttons.getChildren().addAll(save, display);
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setSpacing(10);
+
+        rootGrid.add(input, 0, 0);
+        rootGrid.add(textArea, 0, 1);
+        rootGrid.add(buttons, 0, 2);
         rootGrid.setHgap(15);
         rootGrid.setVgap(15);
         rootGrid.setAlignment(Pos.CENTER);
@@ -62,16 +74,38 @@ public class App extends Application {
         onSaveActionUtil();
     }
 
+    private void prepareDisplayButton() {
+        display = new Button("Display");
+        display.setPrefWidth(100);
+        onDisplayActionUtil();
+    }
+
+    private void prepareTextArea() {
+        textArea = new TextArea();
+        textArea.setPrefWidth(MIN_WIDTH);
+        textArea.setPrefHeight(MIN_WIDTH / 2);
+    }
+
     private void onSaveActionUtil() {
         save.setOnAction(event -> {
             try {
                 savePageContent();
             } catch (IOException e) {
-                e.printStackTrace();
-                setResultMessage(e.getMessage(), STYLE + ";-fx-text-fill: red;");
-                return;
+                textArea.setText(e.getMessage());
             }
-            setResultMessage("Done", STYLE + ";-fx-text-fill: green;");
+            textArea.setText("SAVED");
+        });
+    }
+
+    private void onDisplayActionUtil() {
+        display.setOnAction(event -> {
+            try {
+                String pageUrl = field.getText();
+                String content = getPageContent(pageUrl);
+                textArea.setText(content);
+            } catch (IOException e) {
+                textArea.setText(e.getMessage());
+            }
         });
     }
 
@@ -80,12 +114,6 @@ public class App extends Application {
         String content = getPageContent(pageUrl);
         File file = getFileFromChooser();
         saveFile(content, file);
-    }
-
-    private void setResultMessage(String message, String color) {
-        Label doneLabel = new Label(message);
-        doneLabel.setStyle(color);
-        rootGrid.add(doneLabel, 1, 1);
     }
 
     private void saveFile(String content, File file) throws FileNotFoundException {
