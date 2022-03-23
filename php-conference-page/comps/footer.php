4ff82da28ad3583b041a6286708d040cc8d95b88
alexey <oldlifeerror@gmail.com>
2022-03-23 21:47:53 +0200

    Remove useless project 🥲

--- php-conference-page/comps/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-    <div id="footer">
-        <div class="copyright">
-            <p>Copyright © 2019 Bunazov Alexei, All rights reserved</p>
-        </div>
-    </div>
-</body>
-
-</html>
--- php-conference-page/utils/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conexion_id = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
-    
-    if (!$conexion_id)  {
-        die('Connection DB Error: ' .  mysqli_connect_error());
-    }
-	     
-    mysqli_select_db($conexion_id, DB_NAME) or die("DB Selection error!" . mysqli_error($conexion_id));
-?>
